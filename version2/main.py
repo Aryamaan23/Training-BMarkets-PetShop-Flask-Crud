ceca7b6ca5cc6eb2840392e79e841d921d0595b7
ARYAMAAN PANDEY <60667917+Aryamaan23@users.noreply.github.com>
2023-02-05 11:20:16 +0530

    Update main.py

--- version2/main.py
+++ version2/main.py
@@ -1,10 +1,16 @@
 import os
 import psycopg2
 from flask import Flask, render_template,request,redirect,url_for
-from petshop_class import Pet,DBConnection
+#from petshop_class_singleton import Pet,DBConnectionSingleton
 from flask_wtf import FlaskForm
 from wtforms import StringField,BooleanField,SelectField,DateTimeField,TextAreaField,SubmitField,RadioField
 from wtforms.validators import InputRequired, Length
+from petshiosingletonnew import DBConnection
+import logging
+
+
+
+
 
 class EditForm(FlaskForm):
     name=StringField("What is the owner name? ")
@@ -15,6 +21,10 @@ class EditForm(FlaskForm):
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='mysecretkey'
+logging.basicConfig(filename='demo.log',
+level=logging.DEBUG,
+format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+
 
 def get_db_connection():
     conn = psycopg2.connect(host='localhost',
@@ -24,13 +34,20 @@ def get_db_connection():
     return conn
 #When we connect to a remote server 2 more things are required that is port=5432 and ip address
 
+
 db=DBConnection()
+
+try:
+    db.create_table('OwnerSingleton','name','pet_name','pet_type')
+except Exception as e: 
+    pass
+
+
 @app.route('/')
 def index():
-    DBConnection.conndb()
     #owner2=db.create_table('ownerpet23','owner_name','pet_name','pet_breed')
-    owner_details=DBConnection.select_result('owner')
-    DBConnection.close()
+    app.logger.info('Showing Owner Records')
+    owner_details=db.select_result('OwnerSingleton')
     #cur.execute('SELECT * FROM owner;')
     #owner_details = cur.fetchall()
     #cur.close()
@@ -64,10 +81,9 @@ def create():
         pet_name = request.form['pet_name']
         pet_type = request.form['pet_type']
 
-        db.conndb()
-        db.insert_db('owner',name,pet_name,pet_type)
+        db.insert_db('OwnerSingleton',name,pet_name,pet_type)
         #It's important otherwise you won't be able to see the changes!
-        db.close()
+        app.logger.info('New Record Added!')
         return redirect(url_for('index'))
     return render_template('petshopcreate.html')
     """
@@ -80,32 +96,29 @@ def create():
 
 @app.route('/delete/<int:ids>')
 def delete(ids):
-    conn = get_db_connection()
-    curr = conn.cursor()
+    #conn = get_db_connection()
+    #curr = conn.cursor()
     t = (ids,)
-    db.conndb()
-    db.delete_row('owner',t)
+    db.delete_row('OwnerSingleton',t)
+    app.logger.info('Deleting Owner Records')
     #curr.execute('DELETE FROM owner WHERE oid = %s', (ids,))
     #conn.commit()
     #curr.close()
     #conn.close()
-    db.close()
     return redirect(url_for('index'))
 
 
 @app.route('/edit/<int:ids>',methods=["GET","POST"])
 def edit(ids):
-    db.conndb()
-    row=db.fetchrec('owner',ids)
+    #row=db.fetchrec('OwnerSingleton',ids)
     form=EditForm()
     if form.validate_on_submit():
         name=form.name.data
         pet_name=form.pet_name.data
         pet_type=form.pet_type.data
     if request.method=="POST":
-        db.conndb()
-        db.update('owner',name,pet_name,pet_type,ids)
-        db.close()
+        db.update('OwnerSingleton',name,pet_name,pet_type,ids)
+        app.logger.info('Updating Owner Records')
         return redirect(url_for('index'))
     return render_template('editpetshop.html',form=form)
 
@@ -116,6 +129,5 @@ def error():
 
 
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=5000)
